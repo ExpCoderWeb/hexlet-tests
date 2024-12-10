adc07061b7c4a86aa0e8ecbb88fb711c6eeaee77
ExpCoderWeb <shagaev_2011@mail.ru>
2024-12-10 19:55:21 +0400

    add src/capitalize.js

---
+++ src/capitalize.js
@@ -0,0 +1,8 @@
+export default (text) => {
+  if (text === '') {
+    return '';
+  }
+  const [firstChar, ...restSubstring] = text;
+  
+  return `${firstChar.toUpperCase()}${restSubstring.join('')}`;
+};
